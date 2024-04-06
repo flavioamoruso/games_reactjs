73270badbcfd56653f15ca705d9c010337007b53
FLAVIO AMORUSO <flavioamoruso6.com>
2024-04-06 21:18:29 +0200

    inizio card

---
+++ src/components/Game.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export function Giochi(props) {
+  return <></>;
+}
---
+++ src/utils/contents.js
@@ -0,0 +1,44 @@
+const gioco = [
+  {
+    id: "1",
+    title: "Grand Theft Auto",
+    price: "85 €",
+  },
+  {
+    id: "2",
+    title: "Fifa",
+    price: "75 €",
+  },
+  {
+    id: "3",
+    title: "Fortnite",
+    price: "Free",
+  },
+  {
+    id: "4",
+    title: "Mario Bros",
+    price: "30 €",
+  },
+  {
+    id: "5",
+    title: "Call off Duty",
+    price: "65 €",
+  },
+  {
+    id: "6",
+    title: "God of War",
+    price: "50 €",
+  },
+  {
+    id: "7",
+    title: "Spiderman",
+    price: "95 €",
+  },
+  {
+    id: "8",
+    title: "Nba",
+    price: "80 €",
+  },
+];
+
+export default gioco;
