9bdd6a8414e779a1b117090c15768e99723e0cc4
FLAVIO AMORUSO <flavioamoruso6.com>
2024-04-08 21:47:10 +0200

    form impostato

--- src/screen/ContactScreen.js
+++ src/screen/ContactScreen.js
@@ -3,7 +3,37 @@ import React from "react";
 const ContactScreen = () => {
   return (
     <>
-      <h1>Contact</h1>
+      <form class="form">
+        <div style={{ color: "black" }} class="title">
+          Contattaci
+          <br />
+          <span></span>
+        </div>
+        <input
+          type="email"
+          placeholder="Email"
+          name="email"
+          className="input"
+        />
+        <input
+          type="text"
+          placeholder="Nome e cognome"
+          name="name"
+          className="input"
+        />
+        <textarea
+          className="input"
+          placeholder="Di cosa hai bisogno?"
+        ></textarea>
+        <div className="login-with">
+          <div className="button-log"></div>
+          <div className="button-log"></div>
+          <div className="button-log"></div>
+        </div>
+        <button style={{ width: "100px" }} class="btn btn-secondary">
+          Vai →
+        </button>
+      </form>
     </>
   );
 };
