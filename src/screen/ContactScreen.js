d159a7c39f00a3f15953c4b04c036253e10d3345
FLAVIO AMORUSO <flavioamoruso6.com>
2024-04-10 13:56:52 +0200

    form completato

--- src/screen/ContactScreen.js
+++ src/screen/ContactScreen.js
@@ -1,9 +1,25 @@
 import React from "react";
+import { FaLinkedinIn } from "react-icons/fa6";
+import { FaFacebookF } from "react-icons/fa6";
+import { SiTiktok } from "react-icons/si";
 
 const ContactScreen = () => {
   return (
     <>
-      <form class="form">
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          marginTop: "24px",
+          textAlign: "center",
+        }}
+      >
+        <h4>
+          Se desideri più informazioni riguardanti questo sito non eistare a
+          contattarci qui sotto
+        </h4>
+      </div>
+      <form class="form" action="https://formspree.io/f/mrgnolwg" method="POST">
         <div style={{ color: "black" }} class="title">
           Contattaci
           <br />
@@ -24,13 +40,30 @@ const ContactScreen = () => {
         <textarea
           className="input"
           placeholder="Di cosa hai bisogno?"
+          name="message"
         ></textarea>
         <div className="login-with">
-          <div className="button-log"></div>
-          <div className="button-log"></div>
-          <div className="button-log"></div>
+          <div className="button-log">
+            <a href="https://www.linkedin.com/in/flavio-amoruso-fullstackdeveloper/">
+              <FaLinkedinIn size={22} />
+            </a>
+          </div>
+          <div className="button-log">
+            <a href="https://www.facebook.com/">
+              <FaFacebookF size={22} />
+            </a>
+          </div>
+          <div className="button-log">
+            <a href="https://www.tiktok.com/login/phone-or-email">
+              <SiTiktok size={22} />
+            </a>
+          </div>
         </div>
-        <button style={{ width: "100px" }} class="btn btn-secondary">
+        <button
+          type="submit"
+          style={{ width: "100px" }}
+          class="btn btn-secondary"
+        >
           Vai →
         </button>
       </form>
