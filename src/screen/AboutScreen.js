d165c029981525770edd67a2f2a317bc28be447e
FLAVIO AMORUSO <flavioamoruso6.com>
2024-04-11 14:22:31 +0200

    about screen da aggiustare

--- src/screen/AboutScreen.js
+++ src/screen/AboutScreen.js
@@ -1,4 +1,6 @@
 import React from "react";
+import Lottie from "react-lottie";
+import animationData from "../assets/animation/aboutAnimation.json";
 
 const AboutScreen = () => {
   return (
@@ -15,7 +17,37 @@ const AboutScreen = () => {
             possibilita a tutti di visionare i giochi più richiesti
           </p>
         </div>
+        <div className="home-hero-img">
+          <Lottie
+            style={{ marginBottom: "14px" }}
+            className=""
+            options={{
+              loop: true,
+              autoplay: true,
+              reverse: true,
+              animationData,
+            }}
+            width={200}
+            height={200}
+          />
+        </div>
       </div>
+      <section>
+        <h2 style={{ textAlign: "center" }}>Il nostro team</h2>
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "space-around",
+            marginTop: "24px",
+          }}
+        >
+          <div className="bg-1"></div>
+
+          <div className="bg-2"></div>
+
+          <div className="bg-3"></div>
+        </div>
+      </section>
     </>
   );
 };
