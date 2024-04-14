d50cc5bee2752024a742d3abcaea773d6ec639bc
FLAVIO AMORUSO <flavioamoruso6.com>
2024-04-14 15:35:18 +0200

    loader completato

--- src/components/Loading.js
+++ src/components/Loading.js
@@ -3,7 +3,15 @@ import React from "react";
 const Loading = () => {
   return (
     <>
-      <h1>Loading...</h1>
+      <section className="position-spinner">
+        <div class="loadingspinner">
+          <div id="square1"></div>
+          <div id="square2"></div>
+          <div id="square3"></div>
+          <div id="square4"></div>
+          <div id="square5"></div>
+        </div>
+      </section>
     </>
   );
 };
