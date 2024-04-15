86489483e7d1d341bdfe70ef27bc5d2720abca7b
FLAVIO AMORUSO <flavioamoruso6.com>
2024-04-15 13:21:08 +0200

    error screen

--- src/screen/ErrorScreen.js
+++ src/screen/ErrorScreen.js
@@ -1,9 +1,32 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import styled from "styled-components";
+
+const Wrapper = styled.section`
+  min-height: 86.2vh;
+  display: grid;
+  display: --ms-grid;
+  display: --moz-grid;
+  display: --web-kit-grid;
+  place-items: center;
+  h1 {
+    text-transform: uppercase;
+    text-align: center;
+  }
+  @media screen and (min-width: 992px) {
+    min-height: 84.5vh;
+  }
+`;
 
 const ErrorScreen = () => {
   return (
     <>
-      <h1>Pagina non trovata</h1>
+      <Wrapper>
+        <h1>Pagina non trovata</h1>
+        <Link to="/" className="btn btn-primary">
+          Torna indietro
+        </Link>
+      </Wrapper>
     </>
   );
 };
